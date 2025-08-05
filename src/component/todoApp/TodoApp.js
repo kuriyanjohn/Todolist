067f3d9a6f32aa56fe1df345a577b52b1d688c03
Anto jn <kuriyanjohn1953@gmail.com>
2025-08-05 13:53:19 +0530

    edit function added

--- src/component/todoApp/TodoApp.js
+++ src/component/todoApp/TodoApp.js
@@ -5,7 +5,10 @@ export class TodoApp extends Component{
 
     state={
         input:'',
-        items:[]
+        items:[],
+        isOpen:false,
+        editInput:'',
+        editindex:null
     }
     handleChange=event=>{
          this.setState({
@@ -31,21 +34,69 @@ export class TodoApp extends Component{
             items:allItems
         })
     }
+    editItem=index=>{
+            this.setState({
+                isOpen:true,
+                editInput:this.state.items[index],
+                editindex:index
+            })
+    }
+    handleEditChange=event=>{
+        this.setState({
+            editInput:event.target.value
+        })
+    }
+    saveEditedItem=()=>{
+        const {items,editInput,editindex}=this.state
+        const updatedItems=[...items]
+        updatedItems[editindex]=editInput
+        this.setState({
+            items:updatedItems,
+            isOpen:false,
+            editindex:null,
+            editInput:''
+        })
+    }
+    closePopup=()=>{
+        this.setState({
+            isOpen:false,
+            editindex:null,
+            editInput:''
+        })
+    }
     render(){
-        const {input,items}=this.state
+        const {input,items,isOpen,editInput}=this.state
         console.log(items);
         
         return (
             <div className="todo-container">
                 <form className="input-section" onSubmit={this.storeItems}>
-               <h1>todo app </h1>     
+               <h1>todo app </h1>
                
                 <input type="text" value={input} onChange={this.handleChange} placeholder="enter items"/>
                 </form>   
                 <ul>
                     {items.map((data,index)=>(
                         <li key={index}>
-                            {data}<i class='fas fa-trash-alt' onClick={()=>this.deleteItem(index)}></i>
+                            {data}
+                            <i class='fas fa-trash-alt' onClick={()=>this.deleteItem(index)}></i>
+                            <i class="fa-solid fa-pen" onClick={()=>this.editItem(index)}></i>
+                            {isOpen && (
+                                <div className="popup-overlay">
+                                    <div className="popup-content">
+                                    <h2>Edit Item</h2>
+                                    <input
+                                        type="text"
+                                        value={editInput}
+                                        onChange={this.handleEditChange}
+                                    />
+                                    <button onClick={this.saveEditedItem}>Save</button>
+                                    <button onClick={this.closePopup}>Cancel</button>
+                                    </div>
+                                </div>
+                                )}
+
+
                         </li>
                     ))}
                 </ul>                                
